b1004da428d0d1fe836bf3f7889f5e961f6b2c0d
Gobinath Subramaniam <gobinath.dev@gmail.com>
2025-09-24 10:00:00 +0530

    Update SectionChild7Entity.kt

---
+++ app/src/main/java/com/nithra/nithraresume/data/db/entity/SectionChild7Entity.kt
@@ -0,0 +1,32 @@
+package com.nithra.nithraresume.data.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/** Multiple Item Text */
+@Entity(tableName = "section_child_7")
+data class SectionChild7Entity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "section_child_7_id")
+    val sectionChild7Id: Int = 0,
+
+    @ColumnInfo(name = "section_head_added_id")
+    val sectionHeadAddedId: Int,
+
+    @ColumnInfo(name = "sc7_index_position")
+    val sc7IndexPosition: Int,
+
+    @ColumnInfo(name = "sc7_content_title")
+    val sc7ContentTitle: String?,
+
+    @ColumnInfo(name = "sc7_content_subtitle")
+    val sc7ContentSubtitle: String?,
+
+    @ColumnInfo(name = "sc7_content_detail")
+    val sc7ContentDetail: String?,
+
+    @ColumnInfo(name = "sc7_content_detail_bullet_type")
+    val sc7ContentDetailBulletType: String?
+)
+// update 80
