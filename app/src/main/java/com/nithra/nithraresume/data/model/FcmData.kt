b52ddba055ed4844d05092166f1d5a2b8d1babaa
Gobinath Subramaniam <gobinath.dev@gmail.com>
2025-10-06 10:00:00 +0530

    Update FcmData.kt

---
+++ app/src/main/java/com/nithra/nithraresume/data/model/FcmData.kt
@@ -0,0 +1,37 @@
+package com.nithra.nithraresume.data.model
+
+import com.nithra.nithraresume.data.db.entity.FcmDataEntity
+
+data class FcmData(
+    val id: Int = 0,
+    val messageType: String,
+    val notificationType: String,
+    val title: String,
+    val message: String,
+    val imageUrl: String,
+    val timestamp: String,
+    val isRead: Boolean
+)
+
+fun FcmDataEntity.toModel() = FcmData(
+    id = fcmDataId,
+    messageType = fcmMessageType.orEmpty(),
+    notificationType = fcmNotificationType.orEmpty(),
+    title = fcmTitle.orEmpty(),
+    message = fcmMessage.orEmpty(),
+    imageUrl = fcmImageUrl.orEmpty(),
+    timestamp = fcmTimestamp.orEmpty(),
+    isRead = (fcmIsRead ?: 0) == 1
+)
+
+fun FcmData.toEntity() = FcmDataEntity(
+    fcmDataId = id,
+    fcmMessageType = messageType,
+    fcmNotificationType = notificationType,
+    fcmTitle = title,
+    fcmMessage = message,
+    fcmImageUrl = imageUrl,
+    fcmTimestamp = timestamp,
+    fcmIsRead = if (isRead) 1 else 0
+)
+// update 87
