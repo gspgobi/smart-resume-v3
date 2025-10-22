563b8e92f2b47f1a7bc8cbd1c19d92f82f471ece
Gobinath Subramaniam <gobinath.dev@gmail.com>
2025-10-22 10:00:00 +0530

    Update SectionChild7.kt

---
+++ app/src/main/java/com/nithra/nithraresume/data/model/SectionChild7.kt
@@ -0,0 +1,35 @@
+package com.nithra.nithraresume.data.model
+
+import com.nithra.nithraresume.data.db.entity.SectionChild7Entity
+
+/** Multiple Item Text */
+data class SectionChild7(
+    val id: Int = 0,
+    val sectionHeadAddedId: Int,
+    val indexPosition: Int,
+    val contentTitle: String,
+    val contentSubtitle: String,
+    val contentDetail: String,
+    val contentDetailBulletType: String
+)
+
+fun SectionChild7Entity.toModel() = SectionChild7(
+    id = sectionChild7Id,
+    sectionHeadAddedId = sectionHeadAddedId,
+    indexPosition = sc7IndexPosition,
+    contentTitle = sc7ContentTitle.orEmpty(),
+    contentSubtitle = sc7ContentSubtitle.orEmpty(),
+    contentDetail = sc7ContentDetail.orEmpty(),
+    contentDetailBulletType = sc7ContentDetailBulletType.orEmpty()
+)
+
+fun SectionChild7.toEntity() = SectionChild7Entity(
+    sectionChild7Id = id,
+    sectionHeadAddedId = sectionHeadAddedId,
+    sc7IndexPosition = indexPosition,
+    sc7ContentTitle = contentTitle,
+    sc7ContentSubtitle = contentSubtitle,
+    sc7ContentDetail = contentDetail,
+    sc7ContentDetailBulletType = contentDetailBulletType
+)
+// update 95
