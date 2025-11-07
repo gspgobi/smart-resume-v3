07f2172b26959392152a138c13f6d724f7afb8ec
Gobinath Subramaniam <gobinath.dev@gmail.com>
2025-11-07 10:00:00 +0530

    Update ResumeFormatRepository.kt

---
+++ app/src/main/java/com/nithra/nithraresume/data/repository/ResumeFormatRepository.kt
@@ -0,0 +1,29 @@
+package com.nithra.nithraresume.data.repository
+
+import com.nithra.nithraresume.data.db.dao.ResumeFormatBaseDao
+import com.nithra.nithraresume.data.model.ResumeFormat
+import com.nithra.nithraresume.data.model.toEntity
+import com.nithra.nithraresume.data.model.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ResumeFormatRepository @Inject constructor(
+    private val dao: ResumeFormatBaseDao
+) {
+
+    fun getAll(): Flow<List<ResumeFormat>> =
+        dao.getAll().map { list -> list.map { it.toModel() } }
+
+    suspend fun getById(id: Int): ResumeFormat? =
+        dao.getById(id)?.toModel()
+
+    suspend fun getDefault(): ResumeFormat? =
+        dao.getDefault()?.toModel()
+
+    suspend fun update(format: ResumeFormat) =
+        dao.update(format.toEntity())
+}
+// update 103
